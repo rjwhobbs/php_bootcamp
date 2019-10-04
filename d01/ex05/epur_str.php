2ce025a638c33dc868f15ef82fb74d0d821765fb
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2019-10-04 11:21:10 +0200

    Replace multiple spaces with a single space, neat trick

---
+++ d01/ex05/epur_str.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+<?php
+if (isset($argc))
+{
+	if ($argc == 2)
+	{
+		$output = preg_replace("/[ ]+/", ' ', $argv[1]);
+		$trimmed = trim($output, ' ');
+		echo $trimmed . "\n";
+	}
+}
+?>
