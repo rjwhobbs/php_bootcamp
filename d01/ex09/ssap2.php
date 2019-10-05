c8e8e39428632d60b4ba7e3bfd341732c5c4d5e8
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2019-10-05 19:02:39 +0200

    So natsort is weird, one needs to use a for each loop to print the order

--- d01/ex09/ssap2.php
+++ d01/ex09/ssap2.php
@@ -2,8 +2,10 @@
 <?php
 if (isset($argc))
 {
+	$arr;
 	$temp;
 	$nums;
+	$others;
 	$i = 1;
 	$j = 0;
 	while ($i < $argc)
@@ -35,19 +37,13 @@ if (isset($argc))
 			}
 			else
 				$nums[] = $arr[$j];
-			unset($arr[$j]);
-			$temp = array_values($arr);
-			$k = count($arr);
-			for ($l = 0; $l < $k; $l++)
-				unset($arr[$l]);
-			unset($arr);
-			$arr = $temp;
-			$j--;
 		}
 		$j++;
 	}
 	if (isset($nums[0]))
 	{
+		echo "XXXnumsXXX\n";
+		sort($nums);
 		$j = 0;
 		while (isset($nums[$j]))
 		{
@@ -55,8 +51,34 @@ if (isset($argc))
 			$j++;
 		}
 	}
-	for ($i = 0; $i < count($arr); $i++)
-		echo "$arr[$i]\n";
+	// Remove strings with non alnum begining chars.
+	$j = 0;
+	while (isset($arr[$j]))
+	{
+		if (ctype_alnum($arr[$j][0]) === FALSE && is_numeric($arr[$j]) === FALSE)
+		{
+			$others[] = $arr[$j];
+			unset($arr[$j]);
+			$temp = array_values($arr);
+			$k = count($arr);
+			for ($l = 0; $l < $k; $l++)
+			unset($arr[$l]);
+			unset($arr);
+			$arr = $temp;
+			$j--;
+		}
+		$j++;
+	}
+	if (isset($others[0]))
+	{
+		echo "XXXothersXXX\n";
+		natcasesort($others);
+		foreach ($others as $value)
+			echo "$value\n";
+	}
+
+	// for ($i = 0; $i < count($arr); $i++)
+	// 	echo "$arr[$i]\n";
 	// if (ctype_alnum($arr[0]) === FALSE)
 	// 	echo "False";
 	// else
