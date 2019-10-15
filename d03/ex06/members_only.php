b53198cfd245da301190c4e8668a792f5c804be1
Roger HOBBS <rhobbs@c4r6s4.wethinkcode.co.za>
2019-10-15 11:04:09 +0200

    Ahh bootcamp

--- d03/ex06/members_only.php
+++ d03/ex06/members_only.php
@@ -1,17 +1,26 @@
 <?php
-if($_SERVER['PHP_AUTH_USER'] == 'zaz' && $_SERVER['PHP_AUTH_PW'] == 'ilovecheese' ) 
+// if (!isset($_SERVER['PHP_AUTH_USER'])) 
+// {
+//     header('WWW-Authenticate: Basic realm="My Realm"');
+//     header('HTTP/1.0 401 Unauthorized');
+//     echo 'Text to send if user hits Cancel button';
+//     exit;
+// } 
+// else 
+// {
+//     echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
+//     echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
+// }
+if( $_SERVER['PHP_AUTH_USER'] == 'zaz' && $_SERVER['PHP_AUTH_PW'] == '6') 
 {
-	//$_SESSION['login_flag'] = true;
 	echo "howzit";
 } 
 else 
 {
-	echo 'log in bro <br />';
-	// $_SERVER['PHP_AUTH_USER'] = 'admin1';
-	// $_SERVER['PHP_AUTH_PW'] = 'pwd1';
-	header("WWW-Authenticate: Basic realm=\”Schoolroom\”, stale=FALSE");
+	echo 'login bro <br />';
+	header("WWW-Authenticate: Basic realm=\"Memebers only\" ");
 	header("HTTP/1.0 401 Unauthorized");
 	print "<h1>401 Unauthorized</h1>";
-	exit();
+	//exit();
 }
 ?>
