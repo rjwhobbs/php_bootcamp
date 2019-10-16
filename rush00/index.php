0d7accacf0028266919c309ebe0c24cffeefcc9e
Roger HOBBS <rhobbs@c5r9s5.wethinkcode.co.za>
2019-10-16 16:38:08 +0200

    Do you even even

--- rush00/index.php
+++ rush00/index.php
@@ -4,26 +4,26 @@
 	<meta charset="UTF-8">
 	<link href="rush.css" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<title>Fresh Biscuits</title>
+	<title>First We Biscuit</title>
 </head>
-<body><h1>Fresh Biscuits</h1>
-	<!-- <div class="login-menu">
-		<div>Login</div>
-		<div>Register</div>
-		<div>Cart</div>
-	</div><br> -->
-	<div class="menu-bar">
-		<ul>
-			<!-- <li><a href="#">Biscuits</a></li> -->
-			<!-- <li><a href="#">Biscuit Recipes</a></li> -->
-			<li><a href="#">Login</a></li>
-			<li><a href="#">Register</a></li>
-			<li><a href="#">Cart</a></li>
-			<li><a href="#">About Us</a>
-				<div class="sub-menu-1">
-					<ul>
-						<li><a href="mission.php">Our Mission</a></li>
-						<li><a href="contactus.php">Contact Us</a></li>
+<body><h1>But first, we biscuit...</h1>
+<!-- <div class="login-menu">
+	<div>Login</div>
+	<div>Register</div>
+	<div>Cart</div>
+</div><br> -->
+<div class="menu-bar">
+	<ul>
+		<!-- <li><a href="#">Biscuits</a></li> -->
+		<!-- <li><a href="#">Biscuit Recipes</a></li> -->
+		<li><a href="#">Login</a></li>
+		<li><a href="#">Register</a></li>
+		<li><a href="#">Cart</a></li>
+		<li><a href="#">About Us</a>
+		<div class="sub-menu-1">
+			<ul>
+				<li><a href="mission.php">Our Mission</a></li>
+				<li><a href="contactus.php">Contact Us</a></li>
 					</ul>
 				</div>
 			</li>
@@ -31,12 +31,12 @@
 	</div>
 	<br><br><br><br>
 	<div class="products">
-			<div>
-				<div class="cookies-add-cart">
+		<div>
+			<div class="cookies-add-cart">
 				<img class="choc-chip" src="ChocChip.png">
 				<button>Choc-chip Add<i class="fa fa-shopping-cart" style="font-size:24px;"></i>
-				</div>
 			</div>
+		</div>
 			<div>
 				<div class="cookies-add-cart">
 					<img class="peanut-cookie" src="peanut.png">
@@ -52,9 +52,11 @@
 			<div>
 				<div class="cookies-add-cart">
 					<img class="short-bread-choc" src="chocshortbread.png">
-					<button>Choc Shortbread Add<i class="fa fa-shopping-cart" style="font-size:24px;"></i>
+					<button>Choc-shortbread Add<i class="fa fa-shopping-cart" style="font-size:24px;"></i>
 				</div>
 			</div>
-	</div>
+		</div>
+		<br><br><br><br><br>
+	<center><p class="pfooter">freshly baked biscuits delivered daily</p><center>
 </body>
 </html>
